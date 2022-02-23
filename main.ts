e2ca2069796c1866aafb1e384a5ea5d30456bd2b
wiki2022 <100251295+wiki2022@users.noreply.github.com>
2022-02-23 10:48:25 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,19 @@
+let mySprite = sprites.create(img`
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . . . . b b 5 b c 5 5 d 4 c . . 
+    . b b b b 5 5 5 b f d d 4 4 4 b 
+    . b d 5 b 5 5 b c b 4 4 4 4 b . 
+    . . b 5 5 b 5 5 5 4 4 4 4 b . . 
+    . . b d 5 5 b 5 5 5 5 5 5 b . . 
+    . b d b 5 5 5 d 5 5 5 5 5 5 b . 
+    b d d c d 5 5 b 5 5 5 5 5 5 b . 
+    c d d d c c b 5 5 5 5 5 5 5 b . 
+    c b d d d d d 5 5 5 5 5 5 5 b . 
+    . c d d d d d d 5 5 5 5 5 d b . 
+    . . c b d d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
+    `, SpriteKind.Player)
+mySprite.bottom = 120
